a1b43f1b01db8b9c650fc1f40165a52fc8eb1c61
Nova_Max <40432491+tommaier123@users.noreply.github.com>
2019-06-24 15:02:55 +0900

    V1.2.0 Added modifiers and mode
    V1.2.0 Added modifiers and mode

--- SongStatusScript.cs
+++ SongStatusScript.cs
@@ -5,6 +5,7 @@ using Synth.mods.events;
 using System.IO;
 using UnityEngine.SceneManagement;
 using System.Text;
+using System.Collections.Generic;
 
 public class SongStatusScript : ModScript, ISynthRidersEvents
 {
@@ -15,7 +16,18 @@ public class SongStatusScript : ModScript, ISynthRidersEvents
     private string difficulty = "";
     private int n_misses = 0;
     private string misses = "0";
-    private string filePath = null;
+    private string mode = "";
+
+    //modifiers
+    private string accuracy = "NORMAL";
+    private string punchMode = "OFF";
+    private string noteJumpSpeed = "1x";
+    private string oneLife = "OFF";
+    private string obstacles = "ON";
+    private string noteSize = "NORMAL";
+    private string mirrorNotes = "OFF";
+    private string haloNotes = "OFF";
+    private string noFailMode = "OFF";
 
 
     public override void OnModLoaded()
@@ -26,6 +38,7 @@ public class SongStatusScript : ModScript, ISynthRidersEvents
     public override void OnModUnload()
     {
         SceneManager.sceneLoaded -= SceneManager_sceneLoaded;
+        ClearStatus();
     }
 
     private void SceneManager_sceneLoaded(Scene arg0, LoadSceneMode arg1)
@@ -45,13 +58,6 @@ public class SongStatusScript : ModScript, ISynthRidersEvents
 
     public void OnGameStageLoaded(TrackData trackData)
     {
-        //get file path
-        var dataPath = Application.dataPath;
-        filePath = dataPath.Substring(0, dataPath.LastIndexOf('/')) + "/Mods/SongStatusMod/";
-
-        //create directory
-        (new FileInfo(filePath)).Directory.Create();
-
         //get information
         name = trackData.name;
         artist = trackData.artist;
@@ -59,6 +65,7 @@ public class SongStatusScript : ModScript, ISynthRidersEvents
         difficulty = trackData.difficulty;
         misses = "0";
         n_misses = 0;
+        mode = trackData.mode;
         if (trackData.isCustomSong)
         {
             mapper = trackData.beatmapper;
@@ -66,6 +73,66 @@ public class SongStatusScript : ModScript, ISynthRidersEvents
         else
         {
             mapper = "Original";
+        }
+
+        foreach (string modifier in trackData.activemodifiers)//modifiers
+        {
+            if (modifier.Contains("No fail mode"))
+            {
+                noFailMode = "ON";
+            }
+            else if (modifier.Contains("Punch Mode"))
+            {
+                string temp = modifier.Substring(modifier.IndexOf("-") + 2);
+                if (temp.Equals("Normal"))
+                {
+                    punchMode = "NORMAL";
+                }
+                else if (temp.Equals("Hard"))
+                {
+                    punchMode = "HARD";
+                }
+                else if (temp.Equals("Intense"))
+                {
+                    punchMode = "INTENSE";
+                }
+            }
+            else if (modifier.Contains("One Life"))
+            {
+                oneLife = "ON";
+            }
+            else if (modifier.Contains("Accuracity"))
+            {
+                accuracy = "HARD";
+            }
+            else if (modifier.Contains("Note jump speed"))
+            {
+                noteJumpSpeed = modifier.Substring(modifier.IndexOf("-") + 2);
+            }
+            else if (modifier.Contains("Obstacles"))
+            {
+                obstacles = "OFF";
+            }
+            else if (modifier.Contains("Notes size"))
+            {
+                string temp = modifier.Substring(modifier.IndexOf("-") + 2);
+                if (temp.Equals("Small"))
+                {
+                    noteSize = "SMALL";
+                }
+                else if (temp.Equals("Big"))
+                {
+                    noteSize = "BIG";
+                }
+            }
+            else if (modifier.Contains("Mirror Notes"))
+            {
+                mirrorNotes = "ON";
+            }
+            else if (modifier.Contains("Halo Notes"))
+            {
+                haloNotes = "ON";
+            }
         }
         WriteSongStatus();
     }
@@ -110,9 +177,12 @@ public class SongStatusScript : ModScript, ISynthRidersEvents
 
     public void WriteSongStatus()
     {
+        //get file path
+        var dataPath = Application.dataPath;
+        string filePath = dataPath.Substring(0, dataPath.LastIndexOf('/')) + "/Mods/SongStatusMod/";
 
         //load  
-        string text="";
+        string text = "";
         using (StreamReader streamReader = new StreamReader(filePath + "template.txt"))
         {
             text = streamReader.ReadToEnd();
@@ -125,22 +195,31 @@ public class SongStatusScript : ModScript, ISynthRidersEvents
         text = text.Replace("<bpm>", bpm);
         text = text.Replace("<difficulty>", difficulty);
         text = text.Replace("<misses>", misses);
+        text = text.Replace("<mode>", mode);
+
+        //modifiers
+        text = text.Replace("<accuracy>", accuracy);
+        text = text.Replace("<punchMode>", punchMode);
+        text = text.Replace("<noteJumpdSpeed>", noteJumpSpeed);
+        text = text.Replace("<oneLife>", oneLife);
+        text = text.Replace("<obstacles>", obstacles);
+        text = text.Replace("<noteSize>", noteSize);
+        text = text.Replace("<mirrorNotes>", mirrorNotes);
+        text = text.Replace("<haloNotes>", haloNotes);
+        text = text.Replace("<noFailMode>", noFailMode);
 
         //write
         using (StreamWriter streamWriter = new StreamWriter(filePath + "status.txt", false))
-            {
-                streamWriter.WriteLine(text);
-            }
+        {
+            streamWriter.WriteLine(text);
+        }
     }
 
     public void ClearStatus()
     {
         //get file path
         var dataPath = Application.dataPath;
-        filePath = dataPath.Substring(0, dataPath.LastIndexOf('/')) + "/status/";
-
-        //create directory
-        (new FileInfo(filePath)).Directory.Create();
+        string filePath = dataPath.Substring(0, dataPath.LastIndexOf('/')) + "/Mods/SongStatusMod/";
 
         //write
         using (var streamWriter = new StreamWriter(filePath + "status.txt", false))
@@ -156,8 +235,18 @@ public class SongStatusScript : ModScript, ISynthRidersEvents
         difficulty = "";
         n_misses = 0;
         misses = "0";
+        mode = "";
 
-        
+        //modifiers
+        accuracy = "NORMAL";
+        punchMode = "OFF";
+        noteJumpSpeed = "1x";
+        oneLife = "OFF";
+        obstacles = "ON";
+        noteSize = "NORMAL";
+        mirrorNotes = "OFF";
+        haloNotes = "OFF";
+        noFailMode = "OFF";
     }
 }
 
